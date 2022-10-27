e58207484ddc1d07c05aa94779d5b2c09943fe6c
George Golubev <george.gol003@gmail.com>
2022-10-27 12:54:13 +0300

    Забыл добваить тест во вторую задачу

--- 2/main.cpp
+++ 2/main.cpp
@@ -4,12 +4,27 @@
 
 void Test()
 {
-    complex::Complex{1, 1};
-    complex::Complex{2, 2};
+    complex::Complex num1{1, 1};
+    complex::Complex num2{2, 2};
+
+    complex::Complex resSum = complex::sum(num1, num2);
+    complex::Complex resSub = complex::sub(num1, num2);
+    complex::Complex resDiv = complex::div(num1, num2);
+    complex::Complex resMul = complex::mul(num1, num2);
+
+    std::cout << "Сумма = ";
+    complex::print(resSum);
+    std::cout << "Разница = ";
+    complex::print(resSub);
+    std::cout << "Частное = ";
+    complex::print(resDiv);
+    std::cout << "Произведение = ";
+    complex::print(resMul);
 }
 
 int main(int argc, char const *argv[])
 {
+    Test();
     double real, image;
     std::cout << "Введите два числа." << std::endl
               << " 1 - ое число вещественная часть комплексного числа" << std::endl
